1cd3e7e61e67a7744105ed693a23fea6dbf786fb
theHTMLprogrammer <stgupta9935@gmail.com>
2023-03-02 23:22:34 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: false }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: false }));
+
+const CONNECTION_URL = "mongodb+srv://stgupta9935:Pm@310*#ag0t;@cluster0.skeyicj.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`)))
+    .catch((err) => console.log(err.message));
