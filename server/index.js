0a27cf733c6841999872f71aeea016b1c7d1cd2a
theHTMLprogrammer <stgupta9935@gmail.com>
2023-03-13 04:26:31 +0530

    Added dotenv dependency and created system variables

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/postsRouter.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: false }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: false }));
@@ -13,10 +15,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://shashwatgupta:admin@cluster0.n7xnkik.mongodb.net/?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`)))
     .catch((err) => console.log(err.message));
