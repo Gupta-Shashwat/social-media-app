4935ad31ef7a5c9906bf92c2df492d8c371c252c
gupta-shashwat <stgupta9935@gmail.com>
2023-05-17 00:04:32 +0530

    Added pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import { Container } from "@material-ui/core";
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Navbar from './Components/Navbar/Navbar.js';
 import Home from './Components/Home/Home.js';
 import Auth from './Components/Auth/Auth.js';
+import PostDetails from "./Components/PostDetails/PostDetails.js";
 
 
 
 const App = () => {
+    const user = JSON.parse(localStorage.getItem('profile'));
     return (
         <BrowserRouter>
-            <Container maxwidth='lg'>
+            <Container maxwidth='xl'>
                 <Navbar />
                 <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route exact path='/auth' component={Auth} />
+                    <Route exact path='/' component={() => <Redirect to="/posts" />} />
+                    <Route exact path='/posts' component={Home} />
+                    <Route exact path='/posts/search' component={Home} />
+                    <Route exact path='/posts/:id' component={PostDetails} />
+                    <Route exact path='/auth' component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
                 </Switch>
             </Container>
         </BrowserRouter >
